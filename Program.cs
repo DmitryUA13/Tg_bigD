61b628c70fab0d4d52c4f14fb7b592774e08ec68
Dmitry13UA <34549353+DmitryUA13@users.noreply.github.com>
2021-02-15 14:58:05 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Tg_bigD
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
